49241e90b2b287e5b4b93c16fd7dee47a939bbc5
Brian Janes <janes.brian@gmail.com>
2023-12-12 11:37:42 -0330

    added add notes modal and had to change a lot of existing functions

--- client/src/components/AddNotesModal.jsx
+++ client/src/components/AddNotesModal.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Modal, Box, Button } from "@mui/material";
 
-const AddNotesModal = () => {
-  return <div>AddNotesModal</div>;
+const AddNotesModal = ({ onSaveNotes, onClose }) => {
+  const [notes, setNotes] = useState("");
+  const textInputRef = useRef(null);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "auto", // Adjust the width as needed
+    bgcolor: "#2f4050",
+    border: "1px solid whitesmoke",
+    borderRadius: "5px",
+    boxShadow: 24,
+    p: 4,
+    display: "flex",
+    flexDirection: "column",
+  };
+  useEffect(() => {
+    // Focus on the text input when the modal is mounted
+    const focusTextInput = () => {
+      if (textInputRef.current) {
+        textInputRef.current.focus();
+      }
+    };
+
+    const timeoutId = setTimeout(focusTextInput, 0); // Introduce a small delay
+
+    return () => clearTimeout(timeoutId); // Cleanup on component unmount
+  }, []);
+
+  const handleDoneClick = () => {
+    onSaveNotes(notes);
+    onClose();
+  };
+
+  return (
+    <Modal open={true} onClose={onClose}>
+      <Box sx={style}>
+        <textarea
+          ref={textInputRef}
+          value={notes}
+          onChange={(e) => setNotes(e.target.value)}
+          placeholder="Type your match notes here."
+          rows={4}
+          cols={50}
+        />
+        <div className="button-div">
+          <Button
+            variant="outlined"
+            onClick={handleDoneClick}
+            style={{
+              width: "4rem",
+              border: "1px solid whitesmoke",
+              color: "whitesmoke",
+            }}
+          >
+            Done
+          </Button>
+        </div>
+      </Box>
+    </Modal>
+  );
 };
 
 export default AddNotesModal;
--- client/src/components/RoundResultsTable.jsx
+++ client/src/components/RoundResultsTable.jsx
@@ -1,7 +1,8 @@
 //
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Button } from "@mui/material";
 import "../App.css";
+import AddNotesModal from "./AddNotesModal";
 
 const RoundResultsTable = ({ round, onRoundDataChange }) => {
   const [data, setData] = useState({
@@ -12,6 +13,8 @@ const RoundResultsTable = ({ round, onRoundDataChange }) => {
     notes: "",
   });
 
+  const [isModalOpen, setModalOpen] = useState(false);
+
   const onChangeInput = (e) => {
     const { name, value } = e.target;
     setData((prevData) => ({
@@ -21,7 +24,6 @@ const RoundResultsTable = ({ round, onRoundDataChange }) => {
           ? Number(value)
           : value,
     }));
-    onRoundDataChange(round, { ...data, [name]: value });
   };
 
   const incrementNumber = (name) => {
@@ -30,13 +32,11 @@ const RoundResultsTable = ({ round, onRoundDataChange }) => {
         ...prevData,
         [name]: prevData[name] + 1,
       }));
-      onRoundDataChange(round, { ...data, [name]: data[name] + 1 });
     } else if (name === "draws" && data[name] < 1) {
       setData((prevData) => ({
         ...prevData,
         [name]: prevData[name] + 1,
       }));
-      onRoundDataChange(round, { ...data, [name]: data[name] + 1 });
     }
   };
 
@@ -46,10 +46,33 @@ const RoundResultsTable = ({ round, onRoundDataChange }) => {
         ...prevData,
         [name]: prevData[name] - 1,
       }));
-      onRoundDataChange(round, { ...data, [name]: data[name] - 1 });
     }
   };
 
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  // Use useCallback to memoize the onRoundDataChange function
+  const memoizedOnRoundDataChange = useCallback(onRoundDataChange, []);
+
+  const handleSaveNotes = (notes) => {
+    setData((prevData) => ({
+      ...prevData,
+      notes: notes,
+    }));
+    handleCloseModal(); // Close the modal after saving notes
+  };
+
+  // Use useEffect with memoizedOnRoundDataChange in the dependency array
+  useEffect(() => {
+    // Call onRoundDataChange when data changes
+    memoizedOnRoundDataChange(round, { ...data, notes: data.notes });
+  }, [data, memoizedOnRoundDataChange, round]);
   return (
     <div className="table-div">
       <table>
@@ -137,9 +160,16 @@ const RoundResultsTable = ({ round, onRoundDataChange }) => {
                   border: "1px solid whitesmoke",
                   height: "2rem",
                 }}
+                onClick={handleOpenModal}
               >
                 Add Notes
               </Button>
+              {isModalOpen && (
+                <AddNotesModal
+                  onSaveNotes={handleSaveNotes}
+                  onClose={handleCloseModal}
+                />
+              )}
             </td>
           </tr>
         </tbody>
